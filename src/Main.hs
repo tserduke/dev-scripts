7932e2f946b0da5d511f8131499d3a07b88dcc99
Taras Serduke <taras.serduke@gmail.com>
2016-09-16 14:00:53 +0300

    reading stack config

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,24 @@
 module Main (main) where
 
+import Data.Maybe
+import Data.Yaml
 import Development.Shake
+import GHC.Generics
+
 
 main :: IO ()
 main = shakeArgs shakeOptions $
-    phony "lint" $
+    phony "lint" $ do
+        config <- readYaml "stack.yaml" :: Action StackConfig
+        need []
         cmd  "echo 1"
+
+
+data StackConfig = StackConfig
+    { packages :: [FilePath]
+    } deriving (Generic, FromJSON)
+
+
+readYaml :: (FromJSON a) => FilePath -> Action a
+readYaml file = need [file] >> liftIO (unwrap <$> decodeFile file) where
+    unwrap = fromMaybe (error $ "readYaml " ++ file)
